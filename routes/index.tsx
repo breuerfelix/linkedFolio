2cbda464d20c791d33052c94860c5f1f9d21cd27
Felix Breuer <fbreuer@pm.me>
2024-04-24 20:21:06 +0200

    modify landing page
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,20 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+    <div
+      class="py-20"
+      style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)"
+    >
+      <div class="container mx-auto px-6">
+        <h2 class="text-4xl font-bold mb-2 text-white">
+          Create your personal Website in 5 Minutes!
+        </h2>
+        <h3 class="text-2xl mb-10 text-gray-200">
+          All content needed will be populated from your LinkedIn Profile.
+        </h3>
+
+        <a href="/login" class="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+          start now
+        </a>
       </div>
     </div>
   );
